2b27a7d2a7367cc71f896f5a9fd2e6942ed2ab47
Prathviraj Prabhu <69745202+PrathvirajPrabhu@users.noreply.github.com>
2020-08-18 10:30:33 +0530

    first commit

---
+++ flask1.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import pytesseract
+from PIL import Image
+import os
+from gtts import gTTS
+
+
+pytesseract.pytesseract.tesseract_cmd = r"D:\tes\TesseractOCR\tesseract.exe"
+
+app = Flask(__name__)
+
+app.config["IMAGE_UPLOADS"] = r"D:\Flask\static\img\uploads"
+@app.route("/home")
+@app.route("/",methods=["GET","POST"])
+def upload_image():
+    if request.method =="POST":
+
+        if request.files:
+
+            image = request.files["image"]
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"],image.filename))
+            print("Image saved")
+            print(image.filename)
+            image_url = r"D:\Flask\static\img\uploads\\" + image.filename
+            image = Image.open(image_url)
+            imageText = pytesseract.image_to_string(image, lang='kan')
+
+        print("Audio playing...")
+
+        myObj = gTTS(text=imageText, lang='kn',slow=False)
+        myObj.save("static/audio/sophistsOCR.mp3")
+        data = imageText
+        return render_template("upload_image.html", data=data)
+
+    else:
+        return render_template("upload_image.html")
+
+if __name__ == "__main__":
+   app.run(debug=True)
