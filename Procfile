7a5684016175d475fbfb7fbae488737c03e33231
Prathviraj Prabhu <69745202+PrathvirajPrabhu@users.noreply.github.com>
2020-09-01 11:51:38 +0530

    flask1.py changes

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn flask1:app
--- flask1.py
+++ flask1.py
@@ -16,8 +16,18 @@ def upload_image():
     if request.method =="POST":
 
         if request.files:
-            language=request.form['select']
-            print(language)
+            l = request.form['select']
+            print(l)
+            if l == "Kannada":
+                language = 'kan'
+            elif l == "English":
+                language = 'eng'
+            elif l == "Hindi":
+                language = 'hin'
+            elif l == "Sanskrit":
+                language = 'san'
+
+
             image = request.files["image"]
             # i = Image.open(image.filename)
             # picture_name = image.filename
@@ -30,11 +40,20 @@ def upload_image():
             # image_url = r"D:\Flask\static\img\uploads\\" + image.filename
             image_url =  os.path.join(app.root_path, 'static/img/uploads', image.filename)
             image = Image.open(image_url)
-            imageText = pytesseract.image_to_string(image, lang='kan')
+            imageText = pytesseract.image_to_string(image, lang=language)
 
         print("Audio playing...")
 
-        myObj = gTTS(text=imageText, lang='kn',slow=False)
+        if l == "Kannada":
+            language = 'kn'
+        elif l == "English":
+            language = 'en'
+        elif l == "Hindi":
+            language = 'hi'
+        elif l == "Sanskrit":
+            language = 'sk'
+
+        myObj = gTTS(text=imageText, lang=language ,slow=False)
         myObj.save("static/audio/sophistsOCR.mp3")
         data = imageText
         return render_template("upload_image.html", data=data)
