4c588c6b444fb2759b589fd91ee189384e665e7b
adigapranava <pranavaadiga18@gmail.com>
2021-05-04 22:55:48 +0530

    option for many languages added

--- flask1.py
+++ flask1.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, jsonify
 import pytesseract
 from PIL import Image
+import json
 import os
 from gtts import gTTS
 from google_trans_new import google_translator
@@ -18,7 +19,6 @@ def getAudioUrl(filename):
 def upload_image():
     if request.method == "POST":
         filename = ""
-        translator = google_translator()
         language = ""
         if request.files:
 
@@ -30,7 +30,7 @@ def upload_image():
                 language = 'kan'
             elif l == "English":
                 language = 'eng'
-            elif l == "Hindi" or l == "Sanskrit":
+            elif l == "Hindi":
                 language = 'hin'
 
             image.save(os.path.join(app.root_path, 'static/img/uploads', filename))
@@ -41,13 +41,11 @@ def upload_image():
             #extracting text from image
             try:
                 imageText = pytesseract.image_to_string(image, lang=language) 
-                translatedText = translator.translate(imageText)
-                if(not(imageText) or not(translatedText)):
+                if(not(imageText)):
                     imageText ="No Text Found"
-                    translatedText = "No Text Found"
             except:
+                print("Error");
                 imageText ="No Text Found"
-                translatedText = "No Text Found"
 
             if l == "Kannada":
                 language = 'kn'
@@ -55,37 +53,13 @@ def upload_image():
                 language = 'en'
             elif l == "Hindi":
                 language = 'hi'
-            elif l == "Sanskrit":
-                language = 'sk'
 
             myObj = gTTS(text=imageText, lang=language, slow=False)
             audioFile = getAudioUrl(filename)
             myObj.save("static/audio/"+audioFile)
-            
-            if l == "Kannada":
-                language = 'kn'
-            elif l == "English":
-                language = 'eng'
-            elif l == "Hindi":
-                language = 'hi'
-            elif l == "Sanskrit":
-                language = 'snk'
-
-            try:
-                kanText = translator.translate(imageText, lang_src=language, lang_tgt = 'kn')
-            except:
-                kanText = translator.translate("Can't convert, Sorry!!", lang_src=language, lang_tgt = 'kn')
-
-            try:
-                hinText = translator.translate(imageText, lang_src=language, lang_tgt = 'hi')
-            except:
-                hinText = translator.translate("Can't convert, Sorry!!", lang_src=language, lang_tgt = 'hi')
 
             obj = {
                 "imgText" : imageText,
-                "engText" : translatedText,
-                "hinText" : hinText,
-                "kanText" : kanText,
                 "audioFile" : audioFile,
             }
 
@@ -94,6 +68,52 @@ def upload_image():
     else:
         return render_template("upload_image.html")
 
+@app.route("/langTranslator", methods=["GET", "POST"])
+def langTranslator():
+    translator = google_translator();
+    if request.method == "POST":
+        data = json.loads(request.data)
+        
+        ORIGINAL_TEXT = data['ORIGINAL_TEXT'];
+        LANG_TO = data['LANG_TO'];
+
+        try:
+            translatedText = translator.translate(ORIGINAL_TEXT, lang_tgt = LANG_TO)
+        except:
+            translatedText = translator.translate("Sorry.. Cant convert at this time :)", lang_src=LANG_FROM, lang_tgt = LANG_TO)
+        return jsonify(
+            {"translatedText" : translatedText}
+        )
+    
+@app.route("/langAvailable", methods=["GET"])
+def langAvailable():
+    return jsonify(
+                {'af': 'afrikaans', 'sq': 'albanian', 'am': 'amharic', 'ar': 'arabic', 'hy': 'armenian', 'az': 'azerbaijani', 'eu': 'basque', 'be': 'belarusian', 'bn': 'bengali', 'bs': 'bosnian', 'bg': 'bulgarian', 'ca': 'catalan', 'ceb': 'cebuano', 'ny': 'chichewa', 'zh-cn': 'chinese (simplified)', 'zh-tw': 'chinese (traditional)', 'co': 'corsican', 'hr': 'croatian', 'cs': 'czech', 'da': 'danish', 'nl': 'dutch', 'en': 'english', 'eo': 'esperanto', 'et': 'estonian', 'tl': 'filipino', 'fi': 'finnish', 'fr': 'french', 'fy': 'frisian', 'gl': 'galician', 'ka': 'georgian', 'de': 'german', 'el': 'greek', 'gu': 'gujarati', 'ht': 'haitian creole', 'ha': 'hausa', 'haw': 'hawaiian', 'iw': 'hebrew', 'hi': 'hindi', 'hmn': 'hmong', 'hu': 'hungarian', 'is': 'icelandic', 'ig': 'igbo', 'id': 'indonesian', 'ga': 'irish', 'it': 'italian', 'ja': 'japanese', 'jw': 'javanese', 'kn': 'kannada', 'kk': 'kazakh', 'km': 'khmer', 'ko': 'korean', 'ku': 'kurdish (kurmanji)', 'ky': 'kyrgyz', 'lo': 'lao', 'la': 'latin', 'lv': 'latvian', 'lt': 'lithuanian', 'lb': 'luxembourgish', 'mk': 'macedonian', 'mg': 'malagasy', 'ms': 'malay', 'ml': 'malayalam', 'mt': 'maltese', 'mi': 'maori', 'mr': 'marathi', 'mn': 'mongolian', 'my': 'myanmar (burmese)', 'ne': 'nepali', 'no': 'norwegian', 'ps': 'pashto', 'fa': 'persian', 'pl': 'polish', 'pt': 'portuguese', 'pa': 'punjabi', 'ro': 'romanian', 'ru': 'russian', 'sm': 'samoan', 'gd': 'scots gaelic', 'sr': 'serbian', 'st': 'sesotho', 'sn': 'shona', 'sd': 'sindhi', 'si': 'sinhala', 'sk': 'slovak', 'sl': 'slovenian', 'so': 'somali', 'es': 'spanish', 'su': 'sundanese', 'sw': 'swahili', 'sv': 'swedish', 'tg': 'tajik', 'ta': 'tamil', 'te': 'telugu', 'th': 'thai', 'tr': 'turkish', 'uk': 'ukrainian', 'ur': 'urdu', 'uz': 'uzbek', 'vi': 'vietnamese', 'cy': 'welsh', 'xh': 'xhosa', 'yi': 'yiddish', 'yo': 'yoruba', 'zu': 'zulu', 'fil': 'Filipino', 'he': 'Hebrew'}
+            )
+   
+
+
+
+# @app.route("/kanTranslated", methods=["GET", "POST"])
+# def kanTranslated():
+#     if l == "Kannada":
+#                 language = 'kn'
+#             elif l == "English":
+#                 language = 'eng'
+#             elif l == "Hindi":
+#                 language = 'hi'
+#             elif l == "Sanskrit":
+#                 language = 'snk'
+
+#             try:
+#                 kanText = translator.translate(imageText, lang_src=language, lang_tgt = 'kn')
+#             except:
+#                 kanText = translator.translate("Can't convert, Sorry!!", lang_src=language, lang_tgt = 'kn')
+
+#             try:
+#                 hinText = translator.translate(imageText, lang_src=language, lang_tgt = 'hi')
+#             except:
+#                 hinText = translator.translate("Can't convert, Sorry!!", lang_src=language, lang_tgt = 'hi')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/upload_image.js
+++ static/js/upload_image.js
@@ -1,11 +1,14 @@
-// var clipboard = new ClipboardJS(".btn");
-// clipboard.on('success', function() {
-//     M.toast({
-//         html: "copied successfully",
-//         classes: "white"
-//     });
-// });
+let WEB_URL = window.location.origin;
 
+var clipboard = new ClipboardJS(".btn");
+clipboard.on('success', function() {
+    M.toast({
+        html: "copied successfully",
+        classes: "white"
+    });
+});
+
+// loading symbol displayer
 function loadingDisplay(params) {
     var formFilled = true;
     document.getElementById("img-form").querySelectorAll("[required]").forEach(function(i) {
@@ -18,3 +21,122 @@ function loadingDisplay(params) {
         document.getElementById("loader").innerHTML = "<div class=\"loader-div\"><div class=\"loader\"><div class=\"square\"></div><div class=\"path\"><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div><p class=\"text-load\">Loading</p></div></div>";
     }
 }
+
+
+//image displayer
+var imgName;
+
+function showPreviewOne(event) {
+    if (event.target.files.length > 0) {
+        let src = URL.createObjectURL(event.target.files[0]);
+        imgName = src;
+        let preview = document.getElementById("file-ip-1-preview");
+        preview.src = src;
+        preview.style.display = "block";
+    } else {
+        myImgRemoveFunctionOne()
+    }
+}
+
+function myImgRemoveFunctionOne() {
+    document.getElementById("file-ip-1-preview").src = "";
+}
+
+//get langset
+// async function getLanguageSet() {
+//     await fetch(WEB_URL + '/langAvailable', {
+//             method: "GET"
+//         })
+//         .then(response => response.json())
+//         .then(json => {
+//             console.log(json);
+//             langSets = json;
+//         });
+// }
+
+async function addLanguages() {
+    let langSets;
+    await fetch(WEB_URL + '/langAvailable', {
+            method: "GET"
+        })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            langSets = json;
+        });
+
+    var langDropdown = document.getElementById("languages");
+    var newOption, optionText;
+    for (x in langSets) {
+        // console.log(x, l[x]);
+
+        // create option using DOM
+        newOption = document.createElement('option');
+        optionText = document.createTextNode(langSets[x]);
+        // set option text
+        newOption.appendChild(optionText);
+        // and option value
+        newOption.setAttribute('value', x);
+
+        newOption.classList.add("p-3");
+        newOption.classList.add("mb-2");
+
+        langDropdown.appendChild(newOption);
+    }
+}
+
+
+// Translated text getter
+ORIGINAL_TEXT = document.getElementById("content").innerText;
+
+async function getTranslatedText(txt, langTo) {
+    let _data = {
+        ORIGINAL_TEXT: txt,
+        LANG_TO: langTo
+    }
+    var translatedText
+
+    await fetch(WEB_URL + '/langTranslator', {
+            method: "POST",
+            body: JSON.stringify(_data),
+            headers: { "Content-type": "application/json; charset=UTF-8" }
+        })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            translatedText = json
+        });
+    return translatedText;
+}
+
+async function translateNow() {
+    var langTo = document.getElementById("languages").value;
+    let _data = {
+        ORIGINAL_TEXT: ORIGINAL_TEXT,
+        LANG_TO: langTo
+    }
+    var translatedText
+
+    await fetch(WEB_URL + '/langTranslator', {
+            method: "POST",
+            body: JSON.stringify(_data),
+            headers: { "Content-type": "application/json; charset=UTF-8" }
+        })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            translatedText = json
+        });
+
+    var dispElement = document.getElementById("tranContainer");
+
+    dispElement.style.display = "block";
+
+    var para = document.getElementById("transText");
+
+    para.innerText = translatedText["translatedText"];
+}
+
+addLanguages();
+
+// Dropdown
